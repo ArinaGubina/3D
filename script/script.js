c36e62ca48701857e84eed56fa0cb3847f39e75e
Arina Gubina <fike-01@mail.ru>
2021-09-04 21:58:55 +0700

    Урок 18 готов

--- script/script.js
+++ script/script.js
@@ -34,17 +34,19 @@ window.addEventListener('DOMContentLoaded', () => {
                 clearInterval(intervalIndex);
             }
         }
+
+        // Что-то я слегка отстала. Надо подсчитать, сколько времени осталось на выполнение оставшихся уроков :)
         const lessons = 17,
             points = 26;
 
         setTimeout(() => {
             const timerDiploma = getTimeRemaining(deadLine),
-                h = (timerDiploma.hours / 10 < 1) ? '0' + timerDiploma.hours : timerDiploma.hours,
-                m = (timerDiploma.minutes / 10 < 1) ? '0' + timerDiploma.minutes : timerDiploma.minutes,
+                h = timerDiploma.hours + 11,
+                m = timerDiploma.minutes,
                 t = (h * 60 + m) / (57 - points),
                 timer30 = getTimeRemaining('9 september 2021'),
-                h30 = (timer30.hours / 10 < 1) ? '0' + timer30.hours : timer30.hours,
-                m30 = (timer30.minutes / 10 < 1) ? '0' + timer30.minutes : timer30.minutes,
+                h30 = timer30.hours,
+                m30 = timer30.minutes,
                 t30 = (h30 * 60 + m30) / (30 - lessons);
             console.log('Время на 1 урок: ' + Math.floor(t30 / 60) + ':' + Math.floor(t30 % 60));
             console.log('Время на 1 балл: ' + Math.floor(t / 60) + ':' + Math.floor(t % 60));
@@ -52,4 +54,55 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     countTimer('13 september 2021');
+
+    // меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
+
+    //popup
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+        let popupInterval,
+            left;
+        const popupAnimate = () => {
+            popupInterval = requestAnimationFrame(popupAnimate);
+            if (left > 39) {
+                left -= 5 - 0.073 * (105 - left);
+                popupContent.style.left = left + '%';
+            } else {
+                cancelAnimationFrame(popupInterval);
+            }
+        };
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (window.screen.width >= 768) {
+                    left = 105;
+                    popupContent.style.left = left + '%';
+                    popupInterval = requestAnimationFrame(popupAnimate);
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+    togglePopup();
 });
