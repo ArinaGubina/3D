36eaefece66f702929b2d1bfb7acac144733afb8
Arina Gubina <fike-01@mail.ru>
2021-09-04 15:31:46 +0700

    Незадокументированные возможности консольки

--- script/script.js
+++ script/script.js
@@ -5,8 +5,8 @@ window.addEventListener('DOMContentLoaded', () => {
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
-            const dateStop = new Date(deadLine).getTime(),
+        function getTimeRemaining(deadLineArg) {
+            const dateStop = new Date(deadLineArg).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
                 seconds = Math.floor(timeRemaining % 60),
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 1000);
 
         function updateClock() {
-            const timer = getTimeRemaining();
+            const timer = getTimeRemaining(deadLine);
             timerHours.textContent = (timer.hours / 10 < 1) ? '0' + timer.hours : timer.hours;
             timerMinutes.textContent = (timer.minutes / 10 < 1) ? '0' + timer.minutes : timer.minutes;
             timerSeconds.textContent = (timer.seconds / 10 < 1) ? '0' + timer.seconds : timer.seconds;
@@ -34,6 +34,21 @@ window.addEventListener('DOMContentLoaded', () => {
                 clearInterval(intervalIndex);
             }
         }
+        const lessons = 17,
+            points = 26;
+
+        setTimeout(() => {
+            const timerDiploma = getTimeRemaining(deadLine),
+                h = (timerDiploma.hours / 10 < 1) ? '0' + timerDiploma.hours : timerDiploma.hours,
+                m = (timerDiploma.minutes / 10 < 1) ? '0' + timerDiploma.minutes : timerDiploma.minutes,
+                t = (h * 60 + m) / (57 - points),
+                timer30 = getTimeRemaining('9 september 2021'),
+                h30 = (timer30.hours / 10 < 1) ? '0' + timer30.hours : timer30.hours,
+                m30 = (timer30.minutes / 10 < 1) ? '0' + timer30.minutes : timer30.minutes,
+                t30 = (h30 * 60 + m30) / (30 - lessons);
+            console.log('Время на 1 урок: ' + Math.floor(t30 / 60) + ':' + Math.floor(t30 % 60));
+            console.log('Время на 1 балл: ' + Math.floor(t / 60) + ':' + Math.floor(t % 60));
+        }, 3000);
     }
 
     countTimer('13 september 2021');
