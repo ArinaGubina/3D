9bd17623d4dc04221ec46b150d79d50252cdd0b4
Arina Gubina <fike-01@mail.ru>
2021-09-06 12:53:13 +0700

    Урок 22 готов

--- script/script.js
+++ script/script.js
@@ -60,22 +60,22 @@ window.addEventListener('DOMContentLoaded', () => {
         const menu = document.querySelector('menu'),
             body = document.querySelector('body');
 
-        const scrollTo = event => {
-            event.preventDefault();
-            const hr = event.target.closest('a[href]');
-            if (hr) {
+        const scrollTo = hr => {
+            try {
                 const section = document.querySelector(hr.getAttribute('href'));
                 if (section) {
                     section.scrollIntoView({ block: "start", behavior: "smooth" });
                 }
-            }
+            } catch { return; }
         };
         const handlerMenu = event => {
             if (event.target.closest('.menu') || event.target.closest('menu')) {
                 menu.classList.toggle('active-menu');
             }
-            if (!event.target.closest('.menu')) {
-                scrollTo(event);
+            const hr = event.target.closest('a[href]');
+            if (hr) {
+                event.preventDefault();
+                scrollTo(hr);
             }
         };
         body.addEventListener('click', event => {
@@ -161,4 +161,82 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     tabs();
+
+    //слайдер
+    const slider = () => {
+        const portfolioDots = document.querySelector('.portfolio-dots'),
+            slider = document.querySelector('.portfolio-content'),
+            slide = document.querySelectorAll('.portfolio-item');
+        const insertDots = () => {
+            let insertHtml = `<li class="dot dot-active"></li>`;
+            if (slide.length > 0) {
+                for (let i = 1; i < slide.length; i++) {
+                    insertHtml += `<li class="dot"></li>`;
+                }
+                portfolioDots.insertAdjacentHTML('afterbegin', insertHtml);
+            }
+        };
+        let currentSlide = 0,
+            interval,
+            // eslint-disable-next-line prefer-const
+            dot;
+
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide = (currentSlide + 1) % slide.length;
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+        const startSlide = (time = 3000) => {
+            interval = setInterval(() => autoPlaySlide(), time);
+        };
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('click', event => {
+            event.preventDefault();
+            const target = event.target;
+            if (!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            if (target.matches('#arrow-right')) {
+                currentSlide = (currentSlide + 1) % slide.length;
+            } else if (target.matches('#arrow-left')) {
+                currentSlide = (currentSlide - 1 + slide.length) % slide.length;
+            } else if (target.matches('.dot')) {
+                dot.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        });
+
+        slider.addEventListener('mouseover', event => {
+            if (event.target.matches('.portfolio-btn, .dot')) {
+                stopSlide();
+            }
+        });
+        slider.addEventListener('mouseout', event => {
+            if (event.target.matches('.portfolio-btn, .dot')) {
+                startSlide();
+            }
+        });
+        insertDots();
+        dot = document.querySelectorAll('.dot');
+        startSlide(1500);
+    };
+    slider();
 });
